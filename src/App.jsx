b4efeda537c48daa0af380bebc5f8197a2c757f8
Sheriff Aderombi <ayaderombi@gmail.com>
2024-10-26 23:10:56 +0100

    personal practice

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import List from './List.jsx'
 
 
 function App() {
-  const [people, setPeople] = useState(data)
+  const [celebrants, setCelebrants] = useState(data)
   return (
     <main>
       <section className="container">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people}/>
-        <button onClick={() => setPeople([])}>clear all</button>
+        <h3>{celebrants.length} birthdays today</h3>
+        <List celebrants={celebrants}/>
       </section>
     </main>
     
--- src/List.jsx
+++ src/List.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-const List = (props) => {
+const List = ({celebrants}) => {
         return (
-            props.people.map((person) => {
-                const {id, name, image, age} = person
-                return (
-                    <article className="person">
-                        <img src={image} alt={name} />
-                        <div>
-                            <h4>{name}</h4>
-                            <p>{age} years</p>
-                        </div>
-                    </article>
-                )
-            })
+               <section>
+                 {celebrants.map((celebrant) => {
+                    const {id, name, age, image} = celebrant;
+                    return (
+                        <article className="person" key={id}>
+                            <img src={image} alt={name} />
+                            <div>
+                                <h4>{name}</h4>
+                                <p>{age} years</p>
+                            </div>
+                        </article>
+                    )
+                 })}
+               </section>
         )
         }
     
